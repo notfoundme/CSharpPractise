4505900c76327da79f731e6604de6aeccbe22abf
notfoundme <garimabhattarai059@gmail.com>
2024-01-19 08:16:51 +0545

    Example2: Hisab Machine

--- CSharpLearning/Program.cs
+++ CSharpLearning/Program.cs
@@ -1,19 +1,42 @@
-﻿// WAP to create a array of string having students name
-// and print them
+﻿
 using System;
 
 namespace StudentProgram {
     class Program {
         static void Main(string[] args)
         {
-            Example1();
-            Example1();
-            Example1();
-            Example1();
-            Example1();
+            Example2();
+        }
+
+        static void Example2() {
+            // WAP to create a array of money i earned this week
+            // print average money i earned per day
+            // monthly income based on average earning
+            // yearly income based on average earning
+            Double[] salary = new double[7] { 100, 50, 23, 20, 25, 24, 28 };
+            Double sum = 0;
+            foreach (Double item in salary) {
+                sum += item;
+            }
+
+            Double average = sum / 7;
+            Console.WriteLine("---------------------- The Hisab Machine ----------------------");
+            Console.WriteLine("");
+            Console.WriteLine("---------------------- The Average for each day ----------------------");
+            Console.WriteLine(average);
+            Console.WriteLine("---------------------- The Average for this month ----------------------");
+            Console.WriteLine(average * 30);
+            Console.WriteLine("---------------------- The Average for this year ----------------------");
+            Console.WriteLine(average * 30 * 12);
+            Console.WriteLine("---------------------- Total Earning ----------------------");
+            Console.WriteLine(sum);
+            Console.WriteLine("---------------------- Thank You ----------------------");
         }
 
         static void Example1() {
+            // WAP to create a array of string having students name
+            // and print them
+
             string[] students = new string[5] { "Sulav", "Garima", "upasana", "tesula", "udaya" };
 
             // using for each loop
