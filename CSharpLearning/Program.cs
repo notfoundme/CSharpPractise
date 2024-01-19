25a54b5e567d0c60687dbf0bbdea7c7ef72ec2e4
notfoundme <garimabhattarai059@gmail.com>
2024-01-19 08:05:16 +0545

    Example1: String and Loops

---
+++ CSharpLearning/Program.cs
@@ -0,0 +1,42 @@
+﻿// WAP to create a array of string having students name
+// and print them
+using System;
+
+namespace StudentProgram {
+    class Program {
+        static void Main(string[] args)
+        {
+            Example1();
+            Example1();
+            Example1();
+            Example1();
+            Example1();
+        }
+
+        static void Example1() {
+            string[] students = new string[5] { "Sulav", "Garima", "upasana", "tesula", "udaya" };
+
+            // using for each loop
+            foreach (string x in students)
+            {
+                Console.WriteLine(x);
+            }
+
+            // using for loop
+            for (int x = 0; x < students.Length; x++)
+            {
+                Console.WriteLine(students[x]);
+            }
+
+            int m = 0;
+            while (m < students.Length)
+            {
+                Console.WriteLine(students[m]);
+
+                m++;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
